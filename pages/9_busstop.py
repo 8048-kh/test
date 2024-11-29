c5a36b355ef28007581a8759f3c75236442c03d6
8048-kh <8046khtotn@gmail.com>
2024-11-29 18:50:32 +0800

    Update 9_busstop.py

--- pages/9_busstop.py
+++ pages/9_busstop.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -14,16 +15,23 @@ logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
 st.title("Heatmap")
+bus_stops = pd.read_csv("https://github.com/8048-kh/GIS-files/raw/refs/heads/main/CSV/Kaohsiung%20Bus%20Stop.csv")
+
+for index, row in bus_stops.iterrows():
+    township = row["TOWNNAME"]  # 將 "township" 替換為實際欄位名稱
+    if township in bus_stop_counts:
+        bus_stop_counts[township] += 1
+    else:
+        bus_stop_counts[township] = 1
 
 with st.expander("See source code"):
     with st.echo():
-        filepath = "https://github.com/8048-kh/GIS-files/raw/refs/heads/main/CSV/Kaohsiung%20Bus%20Stop.csv"
         m = leafmap.Map(center=[40, -100], zoom=4)
         m.add_heatmap(
             filepath,
             latitude="Lat",
             longitude="Lon",
-            value="TOWNNAME",
+            value="bus_stop_counts",
             name="Heat map",
             radius=20,
         )
