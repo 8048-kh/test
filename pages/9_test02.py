9b2d5fda7e8be393dc3e5428090737888e886fff
8048-kh <8046khtotn@gmail.com>
2024-11-29 15:57:03 +0800

    Update 9_test02.py

--- pages/9_test02.py
+++ pages/9_test02.py
@@ -9,7 +9,7 @@ A Streamlit map template
 """
 
 st.sidebar.info(markdown)
-busroute = "https://github.com/8048-kh/GIS-files/blob/main/Line/Kaohsiung_Bus_Route.geojson"
+debris = "https://github.com/8048-kh/test/blob/main/debris.geojson"
 
 
 st.title("Split-panel Map")
@@ -18,7 +18,7 @@ with st.expander("See source code"):
     with st.echo():
         m = leafmap.Map()
         m.split_map(
-            left_layer="busroute", right_layer="ESA WorldCover 2020"
+            left_layer="debris", right_layer="ESA WorldCover 2020"
         )
         m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
 
