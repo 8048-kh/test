28af1d9e1eec3f1083f15e473ad7c5ed07658fc2
8048-kh <8046khtotn@gmail.com>
2024-11-29 18:03:49 +0800

    Update 9_test02.py

--- pages/9_test02.py
+++ pages/9_test02.py
@@ -10,13 +10,11 @@ A Streamlit map template
 
 st.sidebar.title("About")
 st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
-st.sidebar.image(logo)
-
 st.title("Split-panel Map")
 
 with st.expander("See source code"):
     with st.echo():
+        
         m = leafmap.Map(center=[23.97565, 120.9738819], zoom=4)
         debris = "https://github.com/8048-kh/test/raw/refs/heads/main/debris1736_20240611_twd97_UTF8.shp"
         Route = "https://github.com/8048-kh/test/raw/refs/heads/main/Bus%20Route.shp"
@@ -24,8 +22,8 @@ with st.expander("See source code"):
         m.add_shp(Route, layer_name="Route")
         m.add_shp(debris, layer_name="debris")
         m.split_map(
-            left_layer = debris,
-            right_layer = Route 
+            left_layer = "https://github.com/8048-kh/test/raw/refs/heads/main/debris1736_20240611_twd97_UTF8.shp",
+            right_layer = "https://github.com/8048-kh/test/raw/refs/heads/main/Bus%20Route.shp"
         )
         m.add_legend(title="debris", builtin_legend="Route")
 
