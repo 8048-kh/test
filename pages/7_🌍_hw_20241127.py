618b356c5f31b45e614a71299924670c0ffe4793
8048-kh <8046khtotn@gmail.com>
2024-11-29 17:42:37 +0800

    Update 7_🌍_hw_20241127.py

--- pages/7_🌍_hw_20241127.py
+++ pages/7_🌍_hw_20241127.py
@@ -27,7 +27,7 @@ with st.expander("See source code"):
             cities,
             x="longitude",
             y="latitude",
-            color_column="COUNTYNAME",
+            #color_column="COUNTYNAME",
             icon_names=["gear", "map", "leaf", "globe"],
             spin=True,
             add_legend=True,
