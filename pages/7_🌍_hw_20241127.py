e29090e0311cb618972b2de8c5c46a158d22a4a6
8048-kh <8046khtotn@gmail.com>
2024-11-29 17:37:21 +0800

    Update 7_🌍_hw_20241127.py

--- pages/7_🌍_hw_20241127.py
+++ pages/7_🌍_hw_20241127.py
@@ -19,18 +19,20 @@ with st.expander("See source code"):
     with st.echo():
 
         m = leafmap.Map(center=[40, -100], zoom=4)
-        cities = "https://github.com/8048-kh/test/raw/refs/heads/main/Aboriginal%20Tribes.csv"
-        regions = "https://github.com/8048-kh/test/blob/main/county.geojson"
+        cities = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
+        regions = "https://github.com/8048-kh/GIS-files/raw/refs/heads/main/Polygon/county/%E8%A1%8C%E6%94%BF%E5%8D%80.shp"
 
-        m.add_geojson(regions, layer_name="county")
+        m.add_shp(regions, layer_name="US Regions")
         m.add_points_from_xy(
             cities,
             x="longitude",
             y="latitude",
-            #color_column="regions",
+            color_column="region",
             icon_names=["gear", "map", "leaf", "globe"],
             spin=True,
             add_legend=True,
         )
+
+m.to_streamlit(height=700)
 
 m.to_streamlit(height=700)
