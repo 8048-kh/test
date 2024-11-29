4dbb260adef5a5bb01da34bfa1023f1dfa1aaece
8048-kh <8046khtotn@gmail.com>
2024-11-29 18:22:58 +0800

    Update 8_rrr.py

--- pages/8_rrr.py
+++ pages/8_rrr.py
@@ -8,12 +8,7 @@ A Streamlit map template
 <https://github.com/opengeos/streamlit-map-template>
 """
 
-st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
-st.sidebar.image(logo)
-
-st.title("Split-panel Map")
+st.title("Minbakiu Bridge Split-panel Map")
 
 with st.expander("See source code"):
     with st.echo():
