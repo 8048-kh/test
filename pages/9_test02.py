499ee39ffa2bc6c546ae1db5dba3d94f3fc0bcfd
8048-kh <8046khtotn@gmail.com>
2024-11-29 16:16:45 +0800

    Update 9_test02.py

--- pages/9_test02.py
+++ pages/9_test02.py
@@ -18,9 +18,9 @@ with st.expander("See source code"):
 
         m = leafmap.Map(center=[40, -100], zoom=4)
         Tribes = "https://github.com/8048-kh/test/raw/refs/heads/main/Aboriginal%20Tribes.csv"
-        debris = "https://github.com/8048-kh/test/blob/main/debris.geojson"
+        #debris = "https://github.com/8048-kh/test/blob/main/debris.geojson"
 
-        m.add_geojson(debris, layer_name="Aboriginal Tribes")
+        m.add_geojson(layer_name="Aboriginal Tribes")
         m.add_points_from_xy(
             Tribes,
             x="longitude",
