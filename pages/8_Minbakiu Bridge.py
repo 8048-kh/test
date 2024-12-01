0728dc608f54da87e0a3b8626cff232b19c8d798
8048-kh <8046khtotn@gmail.com>
2024-12-01 11:11:56 +0800

    Create 8_Minbakiu Bridge.py

---
+++ pages/8_Minbakiu Bridge.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+st.set_page_config(layout="wide")
+
+markdown = """
+A Streamlit map template
+<https://github.com/opengeos/streamlit-map-template>
+"""
+
+st.title("Minbakiu Bridge Split-panel Map")
+
+with st.expander("See source code"):
+    with st.echo():
+        m = leafmap.Map()
+        m.split_map(
+            left_layer="https://github.com/8048-kh/Minbakiu-Bridge/raw/refs/heads/main/09-07-colab.tif",
+            right_layer="https://github.com/8048-kh/Minbakiu-Bridge/raw/refs/heads/main/09-17-colab.tif"
+        )
+        m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
+
+m.to_streamlit(height=700)
