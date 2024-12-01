e701c906110224b53d8d2269ba078928378e0909
8048-kh <8046khtotn@gmail.com>
2024-12-01 09:50:02 +0800

    Update 100_Test.py

--- pages/100_Test.py
+++ pages/100_Test.py
@@ -8,24 +8,23 @@ A Streamlit map template
 <https://github.com/opengeos/streamlit-map-template>
 """
 
-st.title("Aboriginal Tribes")
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
+
+st.title("Heatmap")
 
 with st.expander("See source code"):
     with st.echo():
-
-        m = leafmap.Map(center=[23.97565, 120.9738819], zoom=4)
-        cities = "https://github.com/8048-kh/test/raw/refs/heads/main/Aboriginal%20Tribes.csv"
-        regions = "https://github.com/8048-kh/GIS-files/raw/refs/heads/main/Polygon/county/%E8%A1%8C%E6%94%BF%E5%8D%80.shp"
-
-        m.add_shp(regions, layer_name="Aboriginal Tribes")
-        m.add_points_from_xy(
-            cities,
-            x="longitude",
-            y="latitude",
-            #color_column="COUNTYNAME",
-            icon_names=["gear", "map", "leaf", "globe"],
-            spin=True,
-            add_legend=True,
+        filepath = "https://github.com/8048-kh/test/raw/refs/heads/main/%E8%A6%8B%E8%BB%8A%E7%8E%87_202307.geojson"
+        m = leafmap.Map(center=[40, -100], zoom=4)
+        m.add_heatmap(
+            filepath,
+            latitude="latitude",
+            longitude="longitude",
+            value="category",
+            name="Heat map",
+            radius=20,
         )
-
 m.to_streamlit(height=700)
