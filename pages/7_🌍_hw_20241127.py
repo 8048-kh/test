f95824bb5e5a87129160a0c1a9b52e9f74b48a00
8048-kh <8046khtotn@gmail.com>
2024-12-01 19:08:49 +0800

    Update 7_🌍_hw_20241127.py

--- pages/7_🌍_hw_20241127.py
+++ pages/7_🌍_hw_20241127.py
@@ -15,14 +15,14 @@ with st.expander("See source code"):
 
         m = leafmap.Map(center=[23.97565, 120.9738819], zoom=4)
         cities = "https://github.com/8048-kh/test/raw/refs/heads/main/Aboriginal%20Tribes%20area.csv"
-        regions = "https://github.com/8048-kh/GIS-files/raw/refs/heads/main/Polygon/county/%E8%A1%8C%E6%94%BF%E5%8D%80.shp"
+        regions = "https://github.com/8048-kh/test/raw/refs/heads/main/REGION.shp"
 
         m.add_shp(regions, layer_name="Aboriginal Tribes")
         m.add_points_from_xy(
             cities,
             x="longitude",
             y="latitude",
-            #color_column="COUNTYNAME",
+            color_column="region",
             icon_names=["gear", "map", "leaf", "globe"],
             spin=True,
             add_legend=True,
