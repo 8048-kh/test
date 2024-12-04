a6fe7c9693d854b841b966647efa36efb2942504
8048-kh <8046khtotn@gmail.com>
2024-12-04 14:13:18 +0800

    Create test.py

---
+++ pages/test.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+st.set_page_config(layout="wide")
+
+markdown = """
+A Streamlit map template
+<https://github.com/opengeos/streamlit-map-template>
+"""
+
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
+
+st.title("Heatmap")
+
+with st.expander("See source code"):
+    with st.echo():
+        filepath = "https://github.com/8048-kh/test/raw/refs/heads/main/%E8%A6%8B%E8%BB%8A%E7%8E%87_202307.geojson"
+        m.add_geojson(filepath, layer_name="UBike")
+        m = leafmap.Map(center=[40, -100], zoom=4)
+        m.add_heatmap(
+            filepath,
+            latitude="latitude",
+            longitude="longitude",
+            value="capacity",
+            name="Heat map",
+            radius=20,
+        )
+m.to_streamlit(height=700)
