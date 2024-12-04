5dd623c2af1b44f8dbfe1bac1231e3625c4fb8e4
8048-kh <8046khtotn@gmail.com>
2024-12-04 14:32:27 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -13,18 +13,24 @@ st.sidebar.info(markdown)
 logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
-st.title("Heatmap")
+st.title("Marker Cluster")
 
 with st.expander("See source code"):
     with st.echo():
-        filepath = "https://github.com/8048-kh/test/raw/refs/heads/main/%E8%A6%8B%E8%BB%8A%E7%8E%87_202307.csv"
+
         m = leafmap.Map(center=[40, -100], zoom=4)
-        m.add_heatmap(
-            filepath,
-            latitude="latitude",
-            longitude="longitude",
-            value="capacity",
-            name="Heat map",
-            radius=20,
+        cities = "https://github.com/8048-kh/test/raw/refs/heads/main/activity.csv"
+        #regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
+
+        #m.add_geojson(regions, layer_name="US Regions")
+        m.add_points_from_xy(
+            cities,
+            x="Px",
+            y="Py",
+            #color_column="region",
+            icon_names=["gear", "map", "leaf", "globe"],
+            spin=True,
+            add_legend=True,
         )
+
 m.to_streamlit(height=700)
