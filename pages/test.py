2ff803f560d921b96d5f5e3b26eba55b83b2e6f9
8048-kh <8046khtotn@gmail.com>
2024-12-18 21:09:17 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -21,12 +21,21 @@ with st.expander("See source code"):
         tribe_names = cities_df['部落名稱'].tolist()
 
 # Create a selectbox for tribe names
-        with row2_1:
+        
         selected_tribe = st.selectbox(
             "選擇部落",  # Label for the selectbox
             tribe_names,  # Options for the selectbox (tribe names)
             key="selectbox_tribe"  # Unique key for the selectbox
             )
+        selected_tribe_data = cities_df[cities_df['部落名稱'] == selected_tribe].iloc[0]
+        latitude = selected_tribe_data['latitude']
+        longitude = selected_tribe_data['longitude']
+
+# Update map center
+        m.center = (latitude, longitude)  
+
+# Display the updated map and selected tribe
+        m.to_streamlit(height=700)
 
 # Display the selected tribe
         st.write(f"您選擇的部落是：{selected_tribe}")
