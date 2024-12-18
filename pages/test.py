25f73ac8c52434e2a8562d2365e3c8937a71cb52
8048-kh <8046khtotn@gmail.com>
2024-12-18 20:59:16 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -15,14 +15,27 @@ with st.expander("See source code"):
 
         m = leafmap.Map(center=[23.97565, 120.9738819], zoom=4)
         cities = "https://github.com/8048-kh/test/raw/refs/heads/main/Aboriginal%20Tribes%20area.csv"
-        regions = "https://github.com/8048-kh/GIS-files/raw/refs/heads/main/Polygon/Debris/Debris.shp"
+        regions = "https://github.com/8048-kh/test/raw/refs/heads/main/REGION.shp"
+        cities_df = pd.read_csv(cities_url)
+        tribe_names = cities_df['部落名稱'].tolist()
+
+# Create a selectbox for tribe names
+        selected_tribe = st.selectbox(
+            "選擇部落",  # Label for the selectbox
+            tribe_names,  # Options for the selectbox (tribe names)
+            key="selectbox_tribe"  # Unique key for the selectbox
+            )
+
+# Display the selected tribe
+        st.write(f"您選擇的部落是：{selected_tribe}")
+        
 
         m.add_shp(regions, layer_name="Aboriginal Tribes")
         m.add_points_from_xy(
             cities,
             x="longitude",
             y="latitude",
-            #color_column="region",
+            color_column="region",
             icon_names=["gear", "map", "leaf", "globe"],
             spin=True,
             add_legend=True,
